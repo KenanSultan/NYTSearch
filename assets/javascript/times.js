5abf024c8d652599f717ab9bc65e8d051a76504d
KenanSultan <kenansultan@hotmail.com>
2019-04-05 17:07:31 +0400

    searching for year

--- assets/javascript/times.js
+++ assets/javascript/times.js
@@ -3,26 +3,30 @@ var qurl = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
 var apikey = "LICjtqANAEWiEFnRRv38UUwE6hqGOKTE"
 
 $("#search-btn").on("click", function () {
+    console.log("ok")
 
-var searchInput = $("#search-term").val()
+    var searchInput = $("#search-term").val()
 
-var number = $("#sel1").val()
+    var number = $("#sel1").val()
 
-var startYear = $("#start-year").val()
+    var startYear = $("#start-year").val()
 
-var endYear = $("#end-year").val()
-$ajax ({
-    url: qurl,
-    data: {
-        q: searchInput,
-        "api-key": apikey
-    }
-}).done (function (response) {
-    console.log (response)
+    var endYear = $("#end-year").val()
+
+    $.ajax({
+        url: qurl,
+        data: {
+            q: searchInput,
+            "api-key": apikey,
+            "begin_date": startYear + "0101",
+            "end_date": endYear + "0101"
+        }
+    }).done(function (response) {
+        console.log(response)
+    })
+
 })
 
-})
-
-$("#clear-btn").on("click", function(){
+$("#clear-btn").on("click", function () {
     $(".articles").empty()
 })
